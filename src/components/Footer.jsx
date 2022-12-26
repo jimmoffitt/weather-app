57be2bb0ce52d20b7358a7191eca18111309a76c
@snowman <jimmoffitt@yahoo.com>
2022-12-26 09:19:02 -0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,6 +11,7 @@ function Footer() {
   return (
     <>
       <footer className="footer">
+        This project is based on https://github.com/hicodersofficial/weather-app (see below)
         <a
           href="https://github.com/jimmoffitt/weather-app"
           target="_blank"
@@ -29,6 +30,33 @@ function Footer() {
         </a>
         <a
           href="https://github.com/jimmoffitt/weather-app"
+          target="_blank"
+          rel="noreferrer"
+          style={isDark ? anchorStyle : null}
+        >
+          <i class="fa-solid fa-star"></i> <span>Star</span>
+        </a>
+      </footer>
+      <footer className="footer">
+      https://github.com/hicodersofficial/weather-app:
+        <a
+          href="https://github.com/hicodersofficial/weather-app"
+          target="_blank"
+          rel="noreferrer"
+          style={isDark ? anchorStyle : null}
+        >
+          <i class="fa-brands fa-github"></i> <span>GitHub</span>
+        </a>
+        <a
+          href="https://github.com/hicodersofficial/weather-app"
+          target="_blank"
+          rel="noreferrer"
+          style={isDark ? anchorStyle : null}
+        >
+          <i class="fa-solid fa-code-fork"></i> <span>Fork</span>
+        </a>
+        <a
+          href="https://github.com/hicodersofficial/weather-app"
           target="_blank"
           rel="noreferrer"
           style={isDark ? anchorStyle : null}
