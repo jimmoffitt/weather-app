aa8604048a8eb4bbd52a27c716f28d26a429e22f
@snowman <jimmoffitt@yahoo.com>
2022-12-26 11:28:08 -0700

    Updating app icon

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import AppContext from "../provider/appContext";
+import AppContext from "../provider/weatherAppContext";
 import Loader from "./Loader";
 import Temperature from "./Temperature";
 
--- src/hooks/useTemp.js
+++ src/hooks/useTemp.js
@@ -1,10 +1,10 @@
 import { useContext } from "react";
-import AppContext from "../provider/appContext";
+import weatherAppContext from "../provider/weatherAppContext";
 
 function useTemp(temp, toFixed = 0) {
   const {
     app: { unit },
-  } = useContext(AppContext);
+  } = useContext(weatherAppContext);
 
   if (unit.toLowerCase() === "f") {
     return ((temp * 9) / 5 + 32).toFixed(toFixed);
