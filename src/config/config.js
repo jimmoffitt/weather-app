f12916598db59acbad4fca0951d9ec05d3f2f9d4
@snowman <jimmoffitt@yahoo.com>
2022-12-26 09:42:43 -0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -5,11 +5,24 @@
  * and follow the instructions there.
  * @link https://home.openweathermap.org/api_keys
  */
+
+//Pull in authentication details from local environment. 
+
+//OpenWeather API 
 const OPEN_WEATHER_MAP_API_KEY = process.env.REACT_APP_API_KEY;
 
+//Load Twitter authentication tokens.
+const TWITTER_CONSUMER_TOKEN = process.env.TWITTER_CONSUMER_TOKEN;
+const TWITTER_USER_TOKEN = process.env.TWITTER_USER_TOKEN;
+
+//Provide the paths to external data services. 
 const config = {
+  //OpenWeather API endpoints {Include docs link}
   WEATHER_API_ENDPOINT: `https://api.openweathermap.org/data/2.5/weather?appid=${OPEN_WEATHER_MAP_API_KEY}&`,
   WEATHER_DATA_ENDPOINT: `https://api.openweathermap.org/data/2.5/onecall?appid=${OPEN_WEATHER_MAP_API_KEY}&exclude=minutely&units=metric&`,
+  //Twitter endpoint details
+  TWITTER_FILTERED_STREAM: `https://api.twitter.com/2/tweets/search/stream`,
+  TWITTER_SEARCH_TWEETS: `https://api.twitter.com/2/tweets/search/recent`,
 };
 
 export default config;
